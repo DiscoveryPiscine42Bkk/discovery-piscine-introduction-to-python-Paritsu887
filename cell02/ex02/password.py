ba96f9aba66e14872d052f3fa3441ae028bf98bb
Paritsu Weeruno <wiwiwawa887@gmail.com>
2025-06-25 10:25:52 +0700

    Add files via upload

---
+++ cell02/ex02/password.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+password =  "Python is awesome"
+enterpassword = input("please enter password: ")
+if enterpassword == password:
+  print("ACCESS GRANTED")
+else:
+  print("ACCESS DENIED")
