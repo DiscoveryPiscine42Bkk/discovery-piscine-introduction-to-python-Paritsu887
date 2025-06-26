a43cce74b885df03cbcd6d6efcfb80109d44f759
Paritsu Weeruno <wiwiwawa887@gmail.com>
2025-06-26 14:43:38 +0700

    Update mult.py

--- cell02/ex03/mult.py
+++ cell02/ex03/mult.py
@@ -2,4 +2,11 @@
 x = 0
 first_Number = int(input("Enter the first number: "))
 second_Number = int(input("Enter the second number: "))
-print(first_Number ,"X" , second_Number,"=",first_Number * second_Number)
+result = first_Number * second_Number
+print(first_Number ,"X" , second_Number,"=",result)
+if result == 0:
+    print("This result is positive and negative")
+elif result > 0:
+    print("This result is positive")
+elif result < 0:
+    print("This result is negative")
