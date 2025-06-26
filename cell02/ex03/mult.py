a63654e8f08a22c84b6ce1fcc70826e1e61cdfee
Paritsu Weeruno <wiwiwawa887@gmail.com>
2025-06-26 14:26:17 +0700

    Add files via upload

---
+++ cell02/ex03/mult.py
@@ -0,0 +1,5 @@
+#!/usr/bin/env python3
+x = 0
+first_Number = int(input("Enter the first number: "))
+second_Number = int(input("Enter the second number: "))
+print(first_Number ,"X" , second_Number,"=",first_Number * second_Number)
